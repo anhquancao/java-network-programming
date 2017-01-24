449fbf6352e2d2622dc019117cb07166dfc989e6
Cao Anh Quan <aquancva@gmail.com>
2017-01-24 18:52:38 +0100

    create secure server

--- src/tp3/Constant.java
+++ src/tp3/Constant.java
@@ -7,5 +7,6 @@ public class Constant {
     public static final int PORT = 33333;
     public static final String CHAR_SET = "8859_1";
     public static final int BUFFER_SIZE = 1024;
+    public static final String PASSWORD = "12345678";
 
 }
--- src/tp3/Server.java
+++ src/tp3/Server.java
@@ -1,24 +1,92 @@
 package tp3;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 /**
  * Created by caoquan on 1/24/17.
  */
 public class Server {
     private byte[] digest;
-    private ServerSocket serverSocket;
+    private SSLServerSocket serverSocket;
+
+    public Server(String password) {
+
+        // 1) encryption factory specification
+        KeyManagerFactory kmf = null;
+        SSLContext context = null;
+
+        try {
+            context = SSLContext.getInstance("SSLv3");
+            kmf = KeyManagerFactory.getInstance("SunX509");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        // 2) key manager specification
+        KeyStore ks = null;
+        try {
+            ks = KeyStore.getInstance("JKS");
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+
+        // 3) retrieving the certificate (and the key)
+        char[] passPhrase = password.toCharArray();
+        try {
+            ks.load(new FileInputStream("/Users/caoquan/Documents/workspace/Thread Share/keys/Fichier_Certif"), passPhrase);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+
+        // 4) Parameterization of the encryption factory by the key
+        try {
+            kmf.init(ks, passPhrase);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        }
+
+        // 5) specifying the context for generating SSLServerSocket
+        try {
+            context.init(kmf.getKeyManagers(), null, null);
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+
+        // 6) creation of the SSLServerSocket factory
+        SSLServerSocketFactory factory = context.getServerSocketFactory();
+
+        // 7) create SSLServerSocket
+        try {
+            this.serverSocket = (SSLServerSocket) factory.createServerSocket(Constant.PORT);
+            System.out.println("Create Socket OK");
+        } catch (IOException e) {
+            System.out.println("Error creating server socket:" + e);
+            System.exit(1);
+        }
 
 
-    public Server() {
     }
 
 
     public void listenTocalculateDigest() {
         try {
-            this.serverSocket = new ServerSocket(Constant.PORT);
+//            this.serverSocket = new ServerSocket(Constant.PORT);
             System.out.println("Server is listening at " + Constant.PORT);
             while (true) {
                 Socket socket = serverSocket.accept();
@@ -34,7 +102,7 @@ public class Server {
     }
 
     public static void main(String[] args) {
-        Server server = new Server();
+        Server server = new Server(Constant.PASSWORD);
         server.listenTocalculateDigest();
     }
 }
